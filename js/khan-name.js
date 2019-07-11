f955630fcdfdd1369d3dc249a0b4431d7bef7dcc
IreneMercy <irenemercy700@gmail.com>
2019-07-11 18:40:56 +0300

    add a function to js file that gets the information in the form

---
+++ js/khan-name.js
@@ -0,0 +1,13 @@
+function khanNames(){
+  var year = getElementById('year').inerHTML;
+  var month = getElementById('month').inerHTML;
+  var date = getElementById('date').inerHTML;
+
+}
+khanNames();
+
+function dayWeek(){
+  (d) = (((CC/4) - 2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10))+DD)  mod 7
+  var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; 
+}
