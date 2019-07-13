009d5eae71834775f8de4ea5be4d7cb748357273
IreneMercy <irenemercy700@gmail.com>
2019-07-13 16:33:03 +0300

    add header

--- js/khan-name.js
+++ js/khan-name.js
@@ -1,39 +1,72 @@
-
-document.getElementById('myform').onsubmit =
 function akanName() {
-var birth = document.getElementById('birth').value;
-alert('Your date of birth is ' + birth);
-var CC, YY, MM, DD;
-var CC = dob.slice(0,2);
-var YY = dob.slice(2,4);
-var MM = dob.slice(5,7);
-var DD = dob.slice(8,10);
+  var input = document.getElementById('birth').value;
+  var d = new Date(input);
+  var CC, MM, YY;
+  if (!!d.valueOf()) {
+    YY = d.getFullYear();
+    MM = d.getMonth();
+    CC = d.getDate();
+  }else {/* Invalid date */}
 
+  var male =  ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var female = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday"];
 
-var male= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var dayCalculate = parseInt(((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10))) % 7;
 
-var female = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var gender = document.getElementById('genders').value;
+  console.log(dayCalculate);
+  if (document.getElementById('male').checked) {
+    gender = document.getElementById('male').value;
+    alert(" Since you were born on " + dayOfTheWeek[dayCalculate] + ',' + " your name is " + male[dayCalculate]);
+  }
+  if (document.getElementById('female').checked) {
+    gender = document.getElementById('female').value;
+    alert(" Since you were born on " + dayOfTheWeek[dayCalculate] + ',' + " your name is " + female[dayCalculate]);
+  }
 
-var dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday"];
+  document.getElementById('result').innerHTML = dayOfTheWeek[dayCalculate];
 
-var dayCalculate = parseInt(((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10))) % 7;
-
-var gender = document.getElementById('genders').value;
-    myform.addEventLiatener('submit', function akanName(event)){
-      if (document.getElementById('male').checked) {
-           gender = document.getElementById('male').value;
-    }
-
-       // var maleName = male[dayCalculate - 1]
-       // var day = dayOfTheWeek[dayCalculate -1]
-       // alert(" Since you were born on " + day + " Your name is "+maleName);
-       else if ( gender = document.getElementById('female').checked) {
-        gender = document.getElementById('female').value;
-
-        alert(" Since you were born on " + dayOfTheWeek + " Your name is "+ female);
-           // var femaleName = female[dayCalculate -1]
-           // var day = dayOfTheWeek[dayCalculate - 1]
-           // alert(" Since you were born on " + day + " Your name is "+ femaleName);
-       }
 }
-document.getElementById('result').innerHTML = akanName;
+
+
+
+//
+// document.getElementById('myform').onsubmit =
+// function akanName() {
+// var birth = document.getElementById('birth').value;
+// alert('Your date of birth is ' + birth);
+// var CC, YY, MM, DD;
+// var CC = dob.slice(0,2);
+// var YY = dob.slice(2,4);
+// var MM = dob.slice(5,7);
+// var DD = dob.slice(8,10);
+//
+//
+// var male= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+//
+// var female = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+//
+// var dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday"];
+//
+// var dayCalculate = parseInt(((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10))) % 7;
+//
+// var gender = document.getElementById('genders').value;
+//     myform.addEventLiatener('submit', function akanName(event)){
+//       if (document.getElementById('male').checked) {
+//            gender = document.getElementById('male').value;
+//     }
+//
+//        // var maleName = male[dayCalculate - 1]
+//        // var day = dayOfTheWeek[dayCalculate -1]
+//        // alert(" Since you were born on " + day + " Your name is "+maleName);
+//        else if ( gender = document.getElementById('female').checked) {
+//         gender = document.getElementById('female').value;
+//
+//         alert(" Since you were born on " + dayOfTheWeek + " Your name is "+ female);
+//            // var femaleName = female[dayCalculate -1]
+//            // var day = dayOfTheWeek[dayCalculate - 1]
+//            // alert(" Since you were born on " + day + " Your name is "+ femaleName);
+//        }
+// }
+// document.getElementById('result').innerHTML = akanName;
